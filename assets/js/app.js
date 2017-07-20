32e2d90d71e5ed52ca4c478b8a4a9eaf81017ae2
noemi <noemiarismendi.cc@gmail.com>
2017-07-20 15:14:14 -0500

    Funcionalidad de boton seccion registro lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,44 @@
+//Funcionalidad del carousel
 $('.carousel.carousel-slider').carousel({fullWidth: true});
 
+//Funcionalidad del boton Continuar seccion Registro
+var $telefono = $('#telefonoRegistro');
+var $botonContinuar = $('#botonContinuarRegistro');
+var $checkbox = $('#test5');
+var $digitosTelefono = 0;
+var $terminos = 0;
+
+function validarNumeroRegistro(e){
+    e.preventDefault();
+    var digitosCorrectos = $telefono.val().length;
+    
+    if(digitosCorrectos == 10 && $checkbox.is(":checked")){
+        $botonContinuar.removeClass("disabled");
+    }
+    else{
+        $terminos = false;
+        return $terminos;
+    }
+};
+
+$telefono.keyup(validarNumeroRegistro);
+$checkbox.change(validarNumeroRegistro);
+
+//Funcionalidad de contador en sección Codigo
+var tiempo = 21;
+var contar;
+
+function reintentar(){
+    $("#reinicio").html("Reintentar en"+'<img src="../assets/img/icons/clock.png" class="iconoReloj responsive-img"/>'  + tiempo);
+    tiempo --;
+        if(tiempo == 0){
+        comenzar(contar);
+        location.href = "codigo.html"
+    }
+    contar = setTimeout(reintentar,1000);
+}
+
+function comenzar(contar){
+    clearTimeout(contar)
+}
+$(document).ready(reintentar);
