54ae00d54a8137f94b4359dfa77f4be1b15a3fd6
noemi <noemiarismendi.cc@gmail.com>
2017-07-20 19:15:30 -0500

    maquetacion y cambio de pagina de listo a tarjeta listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,7 +26,7 @@ $checkbox.change(validarNumeroRegistro);
 
 //Funcionalidad de contador en sección Codigo
 var tiempo = 21;
-var contar;
+//var contar;
 
 function reintentar(){
     $("#reinicio").html("Reintentar en"+'<img src="../assets/img/icons/clock.png" class="iconoReloj responsive-img"/>'  + tiempo);
@@ -35,10 +35,41 @@ function reintentar(){
         comenzar(contar);
         location.href = "codigo.html"
     }
-    contar = setTimeout(reintentar,1000);
+    //contar = setTimeout(reintentar,1000);
 }
 
-function comenzar(contar){
+/*function comenzar(contar){
     clearTimeout(contar)
-}
+}*/
 $(document).ready(reintentar);
+
+//Funcionalidad del boton Crear seccion Crear Cuenta
+var $nombre = $('#icon_prefix');
+var $correo = $('#email');
+var $password = $('#password');
+var $botonCrearCuenta = $('#botonCrearCuenta');
+
+
+function validarDatos(){
+    var datoNombre = $nombre.val();
+    var datoMail = $correo.val();
+    var datoContraseña = $password.val();
+    
+    if(datoNombre != " " && datoMail != " " && datoContraseña != " "){
+        $botonCrearCuenta.removeClass("disabled");
+    }
+    else{
+        return alert("Todos los campos son necesarios");
+    }
+};
+
+validarDatos();
+ 
+//Funcionalidad de enviar a otra seccion desde la seccion Registro Listo
+
+function redireccionar(){
+    setTimeout("location.href='../vistas/tarjeta.html'", 3000);
+}
+    
+
+
